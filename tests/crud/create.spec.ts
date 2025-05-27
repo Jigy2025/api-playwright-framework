df86685a9a024f286bd83feaa0807292f280b9fe
unknown <jigyasasahoo06@gmail.com>
2025-05-27 12:32:54 +0530

    message2

--- tests/crud/create.spec.ts
+++ tests/crud/create.spec.ts
@@ -4,5 +4,5 @@ import { api } from '../utils/apiClient';
 test('Create item', async () => {
   const response = await api.post('/items', { name: 'Test Item', price: 10 });
   expect(response.status).toBe(201);
-  expect(response.data.name).toBe('Test Item');
+  expect(response.data.name).toBe('Test Item purpose');
 });
